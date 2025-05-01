cd08031634f105d3b0a6410cd5c469fb613b2ceb
Santi Bouhid <santifbouhid@gmail.com>
2025-05-01 18:03:09 -0300

    Se agrega configuracion de Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:nutrabit_admin/core/router/app-router.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+
+
+
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
