3b0deac2cc1d5a67721d82c276336bb9179a6de8
Santi Bouhid <santifbouhid@gmail.com>
2025-05-08 20:08:02 -0300

    Se agrega diseño pagina login

--- lib/presentation/providers/auth_provider.dart
+++ lib/presentation/providers/auth_provider.dart
@@ -1,13 +1,9 @@
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod/src/notifier.dart';
-
-// NotifierProvider authProvider = NotifierProvider<AuthNotifier>(authProvider.new);
 final authProvider = AsyncNotifierProvider<AuthNotifier, void>(
   () => AuthNotifier(),
 );
-
 class AuthNotifier extends AsyncNotifier<void> {
   @override
   FutureOr<void> build() {
@@ -26,61 +22,11 @@ class AuthNotifier extends AsyncNotifier<void> {
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
+        return null;
       } else if (e.code == 'wrong-password') {
         print('Wrong password provided for that user.');
+        return null;
       }
     }
   }
 }
-// Future<void> registerUser(emailAddress, password) async {
-//   try {
-//     final credential = await FirebaseAuth.instance
-//         .createUserWithEmailAndPassword(
-//           email: emailAddress,
-//           password: password,
-//         );
-//   } on FirebaseAuthException catch (e) {
-//     if (e.code == 'weak-password') {
-//       print('The password provided is too weak.');
-//     } else if (e.code == 'email-already-in-use') {
-//       print('The account already exists for that email.');
-//     }
-//   } catch (e) {
-//     print(e);
-//   }
-// }
-
-// Future<void> loginUser(emailAddress, password, ref) async {
-//   try {
-//     final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-//       email: emailAddress,
-//       password: password,
-//     );
-//   } on FirebaseAuthException catch (e) {
-//     if (e.code == 'user-not-found') {
-//       print('No user found for that email.');
-//     } else if (e.code == 'wrong-password') {
-//       print('Wrong password provided for that user.');
-//     }
-//   }
-// }
-
-
-// final loginAuthProvider = FutureProvider.family<void, (String email, String password)>((ref, credentials) async {
-//   final email = credentials.$1;
-//   final password = credentials.$2;
-
-//   try {
-//     final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-//       email: email,
-//       password: password,
-//     );
-//     // Podés retornar el credential si querés
-//   } on FirebaseAuthException catch (e) {
-//     if (e.code == 'user-not-found') {
-//       print('No user found for that email.');
-//     } else if (e.code == 'wrong-password') {
-//       print('Wrong password provided for that user.');
-//     }
-//   }
-// });
--- lib/presentation/screens/login.dart
+++ lib/presentation/screens/login.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -17,48 +16,113 @@ class _LoginState extends ConsumerState<Login> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double dynamicPadding = screenWidth * 0.20;
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Email',
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: dynamicPadding),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  image: DecorationImage(
+                    image: AssetImage('../assets/img/logoInicio.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Contraseña',
+              const SizedBox(height: 20),
+              Text(
+                'Iniciar Sesión',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                final email = emailController.text;
-                final password = passwordController.text;
-                final cred = ref.read(authProvider.notifier).login(email, password)
-                    .then((value) {
-                  if (value != null) {
-                    context.go('/');
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text('Error al iniciar sesión'),
+              const SizedBox(height: 20),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email',
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Contraseña',
+                ),
+              ),
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.end, // Alinea los elementos a la derecha
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // context.go('/forgot-password');
+                    },
+                    child: Text(
+                      '¿Olvidaste tu contraseña?',
+                      style: TextStyle(
+                        color: Color.fromRGBO(130, 130, 130, 1),
+                        fontSize: 12,
                       ),
-                    );
-                  }
-                });
-                
-              },
-              child: Text("Iniciar Sesión"),
-            ),
-          ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    final email = emailController.text;
+                    final password = passwordController.text;
+                    final cred = ref
+                        .read(authProvider.notifier)
+                        .login(email, password)
+                        .then((value) {
+                          if (value != null) {
+                            context.go('/');
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Error al iniciar sesión'),
+                              ),
+                            );
+                          }
+                        });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(220, 96, 122, 1),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ), // Sin bordes redondeados
+                    ),
+                  ),
+                  child: Text("Iniciar Sesión"),
+                ),
+              ),
+              SizedBox(height: 20),
+               Text('Al hacer clic en continuar, acepta nuestros términos de servicio y nuestra política de privacidad', 
+                  style: TextStyle(
+                    color: Color.fromRGBO(130, 130, 130, 1),
+                    fontSize: 12,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+            ],
+          ),
         ),
       ),
     );
